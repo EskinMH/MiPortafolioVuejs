83b92f4842fc4c1a2f9730df30bcc9bf8e20002e
EskinMH <eskinmontiel5@gmail.com>
2023-06-11 18:45:01 -0600

    final

---
+++ src/components/CardFooter.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <hr class="border-1 my-8 border-gray-400 sm:w-4/12 mx-auto">
+        <div class="text-center">
+            
+            <a href="https://www.facebook.com/" target="_blank"
+                class="m-1 p-1 text-gray-600 text-2xl hover:text-blue-600 dark:text-gray-400"><font-awesome-icon
+                    icon="fa-brands fa-facebook" /></a>
+            <a href="https://www.instagram.com/eskin.mh/" target="_blank"
+                class="m-1 p-1 text-gray-600 text-2xl hover:text-blue-600 dark:text-gray-400"><font-awesome-icon
+                    icon="fa-brands fa-instagram" /></a>
+            <a href="https://github.com/EskinMH/" target="_blank"
+                class="m-1 p-1 text-gray-600 text-2xl hover:text-blue-600 dark:text-gray-400"> <font-awesome-icon
+                    icon="fa-brands fa-github" /></a>
+            <a href="mailto:eskinmontiel5@gmail.com"
+                class="m-1 p-1 text-gray-600 text-2xl hover:text-blue-600 dark:text-gray-400"> <font-awesome-icon
+                    icon="fa-solid fa-envelope" /></a>
+        </div>
+    </div>
+</template>
+<script setup></script>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="text-center">
+        <h2 class="text-center text-xl sm:text-2xl font-bold uppercase dark:text-white">Hola! Soy Eskin Montiel </h2>
+        <p class="sm:text-center text-justify my-4 sm:w-4/12 mx-auto dark:text-gray-300">
+            Me gusta mucho el uso de la tecnologias informaticas actualmente soy un estudiante
+            de la carrera de Ing en sistemas y redes Informaticas en
+            la Universidad Gerardo Barrios, San Miguel, El salvador.
+            actualmente estoy cursando la materia de Programacion Computacional IV </p>
+        <p class="text-center text-blue-600">#DotNetCore #WebApplication</p>
+    </div>
+</template>
+<script setup>
+
+
+</script>
